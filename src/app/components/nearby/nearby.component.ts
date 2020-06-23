995a4f87a2dfd2162f100eede325847e9b934643
Ross Brigoli <rbrigoli@redhat.com>
2020-06-23 11:03:07 +0800

    Nearby API

--- src/app/components/nearby/nearby.component.ts
+++ src/app/components/nearby/nearby.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NearbyService } from '../../services/nearby.service';
+import { Observable } from 'rxjs';
+import { variable } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-nearby',
@@ -11,12 +13,36 @@ export class NearbyComponent implements OnInit {
   constructor(private nearbyService : NearbyService) { }
 
   departures = [];
+  loaded = false;
 
   ngOnInit(): void {
-    this.nearbyService.getDeparturesNearby().subscribe((data: any[]) => {
-      console.log(data);
-      this.departures = data;
-    } )
+    this.nearbyService.getDeparturesNearby().then((data: Observable<any[]>) => {
+      data.subscribe((deps) => {
+        console.log(deps[0]);
+        this.departures = deps[0];
+        this.loaded = true;
+      } );
+    } );
+
+    console.log(this.departures);
+  }
+
+  getTypeIcon(type : string) {
+    if (type.toLowerCase() === "bus")
+      return "directions_bus";
+    else if (type.toLowerCase() === "tram")
+      return "tram";
+    else if (type.toLowerCase() === "train" || type.toLowerCase() == "vline")
+      return "directions_transit";
+  }
+
+  getTypeColor(type: string) {
+    if (type.toLowerCase() === "bus")
+      return "orange";
+    else if (type.toLowerCase() === "tram")
+      return "green";
+    else if (type.toLowerCase() === "train" || type.toLowerCase() == "vline")
+      return "purple";
   }
 
 }
--- src/app/services/nearby.service.ts
+++ src/app/services/nearby.service.ts
@@ -1,23 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { GeolocationService } from './geolocation.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NearbyService {
 
-  private REST_API_SERVER = "/api/search/routes/{latlong}/100";
+  private REST_API_SERVER = "/api/search/routes/{latlong}/200";
 
   constructor(private httpClient: HttpClient, private geoService : GeolocationService) { }
 
-  public getDeparturesNearby() : any {
+  public async getDeparturesNearby() : Promise<any> {
     
-    let lat = -37.8136;
-    let lng = 144.9631;
+    let lat = -37.8183886;
+    let lng = 144.9524854;
+    let response = null;
 
-    //let position = this.geoService.getPosition().then(pos => {lat = pos.lat; lng = pos.lng;});
+    let position = await this.geoService.getPosition()
+      .then(pos => {
+        lat = pos.lat; lng = pos.lng;
+        console.log(lat + "," + lng);    
 
-    return this.httpClient.get(this.REST_API_SERVER.replace("{latlong}", lat + "," + lng));
+        //Override location for testing outside of melbourne
+        //lat = -37.8183886;
+        //lng = 144.9524854;
+
+        response = this.httpClient.get(this.REST_API_SERVER.replace("{latlong}", lat + "," + lng))
+      });
+
+    console.log(lat + "," + lng);    
+
+    return response;
   }
 }
