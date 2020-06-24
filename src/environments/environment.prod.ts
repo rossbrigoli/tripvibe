8e09b852269491536af32ac7e6ba56367a4a28ac
Ross Brigoli <rbrigoli@redhat.com>
2020-06-24 13:14:56 +0800

    ng environment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  nearbyApiURL: "https://routes.tripvibe.apps.r2r.vic.apac.rht-labs.com/api/search/routes/{latlong}/200",
+  nearbyApiURL: "/api/search/routes/{latlong}/200",
   overrideLocation: false
 };
