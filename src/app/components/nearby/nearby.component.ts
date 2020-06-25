297990c804239d94e7dbfa8599c3a0e5fc020fda
Ross Brigoli <rbrigoli@redhat.com>
2020-06-25 10:23:59 +0800

    departure time

--- src/app/components/nearby/nearby.component.ts
+++ src/app/components/nearby/nearby.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NearbyService } from '../../services/nearby.service';
 import { Observable } from 'rxjs';
 import { variable } from '@angular/compiler/src/output/output_ast';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 
 @Component({
   selector: 'app-nearby',
@@ -18,13 +19,13 @@ export class NearbyComponent implements OnInit {
   ngOnInit(): void {
     this.nearbyService.getDeparturesNearby().then((data: Observable<any[]>) => {
       data.subscribe((deps) => {
-        //console.log(deps);
-        this.departures = deps;
+        console.log(deps);
+        this.departures = deps.sort((a, b) => new Date(a.departureTime).valueOf() - new Date(b.departureTime).valueOf());
         this.loaded = true;
       } );
     } );
 
-    console.log(this.departures);
+    this.now = new Date();
   }
 
   getTypeIcon(type : string) {
@@ -45,17 +46,23 @@ export class NearbyComponent implements OnInit {
       return "purple";
   }
 
+  now = new Date();
   refresh(){
     this.loaded = false;
     this.nearbyService.getDeparturesNearby().then((data: Observable<any[]>) => {
       data.subscribe((deps) => {
-        //console.log(deps);
+        console.log(deps);
         this.departures = deps;
         this.loaded = true;
       } );
     } );
+    this.now = new Date();
+  }
 
-    console.log(this.departures);
+  getETA(depTime: string) : string {
+    let eta : number = new Date(depTime).valueOf() - this.now.valueOf();
+    let minETA = Math.ceil(eta / 1000 / 60);
+    return minETA == 0 ? "Now" : minETA.toString();
   }
 
 }
