1b2783a90672a8bc4ccfe15850dde83bbe374102
Henrique Viecili <hviecili@redhat.com>
2020-07-07 16:49:53 +1000

    search now supports querying for past trips

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   nearbyApiURL: "/api/nearby-departures/{latlong}/300?nextHours=4",
   dataEntryApiURL: "/submission",
-  searchApiURL: "/api/search-departures/{term}?routeType={routeType}",
+  searchApiURL: "/api/search-departures/{term}?routeType={routeType}&pastSeconds={pastSeconds}",
   overrideLocation: true
 };
