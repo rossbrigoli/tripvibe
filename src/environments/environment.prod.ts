b1b5df57d70fc4fc2041933d2c2e4767f5948bce
Ross Brigoli <rbrigoli@redhat.com>
2020-07-08 10:44:54 +0800

    added missing config item in environmnet.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   nearbyApiURL: "/api/nearby-departures/{latlong}/{distance}?nextHours={nextHours}&pastHours={pastHours}",
   dataEntryApiURL: "/submission",
   searchApiURL: "/api/search-departures/{term}?routeType={routeType}",
+  stopsApiURL: "/api/stops/route/{route_id}/{route_type}",
   overrideLocation: false
 };
